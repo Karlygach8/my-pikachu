91e63b74434bf6008e92f090677570384c81cdfe
Karlygach8 <karlygash2_008@icloud.com>
2024-07-27 20:49:59 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import Pokemon from './components/Pokemon';
 
 function App() {
+
+  const [pokemonData, setPokemonData] = useState(null);
+
+
+  useEffect(() => {
+    const pokemonNames = ['bulbasaur', 'ivysaur', 'venusaur', 'metapod', 'butterfree', 'weedle'];
+  
+    const fetchPokemon = async (name) => {
+      const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+      const result = await fetch(url);
+      return await result.json();
+    };
+  
+    const fetchData = async () => {
+      const data = await Promise.all(pokemonNames.map(fetchPokemon));
+      setPokemonData(data);
+    };
+  
+    fetchData();
+  }, []);
+
   return (
-    <>
-    </>
+    <div className='Karl'>
+    {pokemonData ? (
+          <div className="card">
+            {pokemonData.map((pokemon, index) => (
+              <div key={index} className="cardii">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <h3 id="name">{pokemon.name}</h3>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+    </div>
   );
 }
 
